2554d8cd49a2776a501eb3de5e056b044ed5972f
bofa <prosit86@gmail.com>
2018-12-01 11:08:51 +0100

    Improvments

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -101,7 +101,7 @@ export default class Main extends Component {
                 </div>
                 
                 <div className="row">
-                    <div className="col-xs-12 col-sm-6">
+                    <div className="col-xs-12 col-md-6">
                         <SelectField value={this.state.fips1} onChange={ this.fips1 }>
                             {this.FIPSData}
                         </SelectField>
@@ -109,7 +109,7 @@ export default class Main extends Component {
                         <Chart year={year} country={this.state.fipsData1} scale={0.1*this.state.fipsData1.get('maxYear')} />
                     </div>
                     
-                    <div className="col-xs-12 col-sm-6">
+                    <div className="col-xs-12 col-md-6">
                         <SelectField value={this.state.fips2} onChange={ this.fips2 } >
                             {this.FIPSData}
                         </SelectField>
